d4c07be724f4b8137830f45f8d99d3a7cda92837
Steffan <git@steffanpadel.eu>
2021-11-21 01:53:23 +0100

    Initial commit, add basic files and methods from previous project

---
+++ images.py
@@ -0,0 +1,35 @@
+from io import BytesIO
+from PIL import Image
+
+
+def scale(right_image, left_image):
+    rw, rh = right_image.size
+    lw, lh = left_image.size
+    if lh > rh:
+        return right_image, left_image.resize((int(lw / lh * rh), rh))
+    elif rh > lh:
+        return right_image.resize((int(rw / rh * lh), lh)), left_image
+    else:
+        return right_image, left_image
+
+
+def create_spread(right_image, left_image):
+    r_scaled, l_scaled = scale(right_image, left_image)
+    rw, rh = r_scaled.size
+    lw, lh = l_scaled.size
+    spread_width = rw + lw
+    spread_height = max(lh, rh)
+    spread_image = Image.new('RGB', (spread_width, spread_height))
+    spread_image.paste(r_scaled, (lw, 0))
+    spread_image.paste(l_scaled, (0, 0))
+    image_io = BytesIO()
+    spread_image.save(image_io, format='JPEG', quality=95)
+    left_image.close()
+    right_image.close()
+    spread_image.close()
+    return image_io.getvalue()
+
+
+def open_image(zipfile, zipinfo):
+    imageb = zipfile.read(zipinfo)
+    return Image.open(BytesIO(imageb))
---
+++ metadata.py
@@ -0,0 +1,11 @@
+
+def get_meta_xml():
+    pass
+
+
+def get_pages(meta_xml):
+    pass
+
+
+def is_manga(meta_xml):
+    pass
