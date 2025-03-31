db310c4bf91eee4cf67f6951c97183684a5b86fb
Steffan <git@steffanpadel.eu>
2025-03-31 23:50:57 +0200

    Switch to pdm, tqdm and 3.13

--- epubtocbz/convert/__init__.py
+++ epubtocbz/convert/__init__.py
@@ -2,8 +2,7 @@ import gc
 from sys import stdout
 from zipfile import ZipFile
 
-from progress.bar import PixelBar
-
+from tqdm import tqdm
 from epubtocbz.convert import internals, images
 
 FORMAT = 'page_{:003}.jpg'
@@ -14,13 +13,14 @@ def process_epub(epub, cbz, make_spreads):
     with ZipFile(epub, 'r') as epub_zip:
         pages = internals.get_spreads_and_pages(epub_zip, make_spreads)
         with ZipFile(cbz, "w") as cbz_zip:
-            pixel_bar = PixelBar(book_name, check_tty=False, file=stdout, empty_fill='⠀')
-            for page in pixel_bar.iter(pages):
+            i = 1
+            for page in tqdm(pages, desc=book_name):
                 if type(page) is tuple:
                     img = images.create_spread(epub_zip.read(page[1]), epub_zip.read(page[0]))
                 else:
                     img = epub_zip.read(page)
-                write_image(cbz_zip, img, pixel_bar.index)
+                write_image(cbz_zip, img, i)
+                i += 1
     gc.collect()
 
 
